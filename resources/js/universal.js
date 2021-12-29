d34570a5276bf127ff29cd4283c7c4c6c2ccb985
Cosillow <silloc@rpi.edu>
2021-12-28 21:07:57 -0500

    added footer and font awesome icons (also added site font according to client)

--- resources/js/universal.js
+++ resources/js/universal.js
@@ -3,6 +3,16 @@ $(document).on('click', '#nav-logo', function(){
   window.location.href = "./index.html";
 });
 
+// add font awesome cdn
+// <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+var cdn = document.createElement('link');
+cdn.rel = "stylesheet";
+cdn.href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css";
+cdn.integrity = "sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==";
+cdn.crossOrigin = "anonymous";
+cdn.referrerPolicy = "no-referrer";
+document.head.append(cdn);
+
 $(document).ready(function() { // after everything in html loads
   $.ajax({
     type: "GET",
@@ -10,6 +20,8 @@ $(document).ready(function() { // after everything in html loads
     dataType: "json",
     cache: "false",
     success: function(responseData, status) {
+      // header
+      // ***************************************************
       var ul = document.createElement("ul");
       var liCon = document.createElement("li");
       liCon.id = "menu";
@@ -31,7 +43,7 @@ $(document).ready(function() { // after everything in html loads
           a.href = value;
           li.append(a);
           ul2.append(li);
-        }             
+        }
       });
       liCon.append(ul2);
       ul.append(liCon);
@@ -40,6 +52,30 @@ $(document).ready(function() { // after everything in html loads
       nav.append(ul);
       header.append(nav);
       document.body.prepend(header);
+      // footer
+      // ***************************************************
+      var nav = document.createElement('nav');
+      var ul = document.createElement('ul');
+      ul.id = "social-list";
+      $.each(responseData.footer.socials, function(i, social) {
+        var li = document.createElement('li');
+        li.className = "social-list";
+        var a = document.createElement('a');
+        a.className = "social-link";
+        var i = document.createElement('i');
+        i.classList = social.icon;
+        a.href = social.link;
+        a.append(i);
+        var span = document.createElement('span');
+        span.append(social.handle);
+        a.append(span);
+        li.append(a);
+        ul.append(li);
+      });
+      nav.append(ul);
+      var footer = document.createElement('footer');
+      footer.append(nav);
+      document.body.append(footer);
     }, error: function(msg) {
       alert("There was a problem: " + msg.status + " " + msg.statusText);
     }
