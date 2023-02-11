8abc00d13323f6481150c57accc3f2f43db36635
Cosillow <silloc@rpi.edu>
2023-02-10 20:37:47 -0500

    Fixed mobile header

--- resources/js/universal.js
+++ resources/js/universal.js
@@ -1,22 +1,6 @@
-$(document).on('click', '#nav-logo', function () {
-  // logo click sends to home
-  window.location.href = "./index.html";
-});
-
-// add font awesome cdn
-// <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-// var cdn = document.createElement('link');
-// cdn.rel = "stylesheet";
-// cdn.href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css";
-// cdn.integrity = "sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==";
-// cdn.crossOrigin = "anonymous";
-// cdn.referrerPolicy = "no-referrer";
-// document.head.append(cdn);
-
-
 // add favicon to all pages
 // <link rel="icon" type="image/png" href="./resources/img/logo-light.png"/>
-var fav = document.createElement('link');
+let fav = document.createElement('link');
 fav.href = "./resources/img/logo-dark.png";
 fav.type = "image/png";
 fav.rel = "icon";
@@ -27,7 +11,6 @@ $(document).ready(function () { // after everything in html loads
   writeFooter(["", "index.html"]);
 });
 
-
 function writeHeader() {
   $.ajax({
     type: "GET",
@@ -35,54 +18,30 @@ function writeHeader() {
     dataType: "json",
     cache: "false",
     success: function (responseData, status) {
-      var ul = document.createElement("ul");
-      var liCon = document.createElement("li");
-      liCon.id = "menu";
-      var ul2 = document.createElement("ul");
-      // add button to close modal (mobile only)
-      var closeLi = document.createElement('li');
-      var closeBt = document.createElement('button');
-      closeBt.innerHTML = "&times;";
-      closeBt.id = "close-modal";
-      closeBt.addEventListener("click", function () {
-        ul2.style.display = "none";
-      });
-      closeLi.append(closeBt);
-      ul2.prepend(closeLi);
+      let ul = document.createElement("ul");
+      ul.id = "menu"
       $.each(responseData.header, function (key, value) { // add internal navagation links to header
-        var li = document.createElement("li");
-        if (key === "nav-logo") {
-          // image logo
-          var img = document.createElement("img");
-          img.src = value;
-          img.id = key;
-          img.alt = "NES-media logo";
-          li.append(img);
-          ul.append(li);
-        } else {
-          // anchor tag
-          var a = document.createElement("a");
-          a.innerHTML = key;
-          a.href = value;
-          li.append(a);
-          ul2.append(li);
-        }
+        let li = document.createElement("li");
+        let a = document.createElement("a");
+        a.innerHTML = key;
+        a.href = value;
+        li.append(a);
+        ul.append(li);
       });
-      liCon.append(ul2);
-      // hambuger button
-      var ham = document.createElement('button');
-      ham.id = "hamburger-button";
-      var hamIcon = document.createElement('i');
-      hamIcon.className = "fas fa-bars";
-      ham.append(hamIcon);
-      ham.addEventListener("click", function () {
-        ul2.style.display = "flex";
-      });
-      liCon.prepend(ham);
-      ul.append(liCon);
-      var nav = document.createElement("nav");
-      var header = document.createElement("header");
+      
+      const hamHTML = '<div class="bar1"></div><div class="bar2"></div><div class="bar3"></div>';
+      let hamBtn = document.createElement("div");
+      hamBtn.innerHTML = hamHTML;
+      hamBtn.id = "hamburger-icon";
+      hamBtn.onclick = function() { 
+        hamBtn.classList.toggle("change");
+        ul.classList.toggle("mobile-visible");
+      };
+
+      let nav = document.createElement("nav");
+      let header = document.createElement("header");
       nav.append(ul);
+      nav.prepend(hamBtn);
       header.append(nav);
       document.body.prepend(header);
     }, error: function (msg) {
