9586776b6472e3d5de96dda9f68f9ff00b025bd9
Cosillow <silloc@rpi.edu>
2023-03-31 11:12:46 -0400

    fixed photos problem with spaces in names

--- resources/js/photography.js
+++ resources/js/photography.js
@@ -2,10 +2,6 @@ $(document).ready(function () {
     loadPictures();
 });
 
-
-
-const dir = "./resources/img/photography/"; 
-=======
 const dir = "./resources/img/photography/";
 
 function loadPictures() {
